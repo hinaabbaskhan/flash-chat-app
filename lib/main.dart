67193a469cc25307620a9e40c1f3b2dfa2f93c44
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-02-16 23:13:37 +0500

    remove theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat_app/screens/chat_screen.dart';
 import 'package:flash_chat_app/screens/login_screen.dart';
 import 'package:flash_chat_app/screens/registration_screen.dart';
@@ -15,11 +16,11 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          bodyMedium: TextStyle(color: Colors.black54),
-        ),
-      ),
+      // theme: ThemeData.dark().copyWith(
+      //   textTheme: TextTheme(
+      //     bodyMedium: TextStyle(color: Colors.black),
+      //   ),
+      // ),
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
