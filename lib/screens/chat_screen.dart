af11eb2e97ed78943890569881abc73701ca5e00
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-02-17 17:16:37 +0500

    message bubble design

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -130,3 +130,52 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+
+class MessageBubble extends StatelessWidget {
+  final String? text;
+  final String? sender;
+  final bool? isMe;
+  MessageBubble(
+      {@required this.text, @required this.sender, @required this.isMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe! ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender!,
+            style: const TextStyle(fontSize: 12, color: Colors.black54),
+          ),
+          Material(
+            elevation: 5.0,
+            borderRadius: isMe!
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  )
+                : const BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                  ),
+            // BorderRadius.circular(30),
+            color: isMe! ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                text!,
+                style: TextStyle(
+                    fontSize: 15, color: isMe! ? Colors.white : Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
